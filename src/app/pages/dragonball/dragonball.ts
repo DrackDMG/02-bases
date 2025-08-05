bf5011c4d3fc0c30fcdf6b1fe3fb57933f4c4e1b
DrackDMG <drackmg@gmail.com>
2025-08-05 17:05:47 -0600

    inputs

--- src/app/pages/dragonball/dragonball.ts
+++ src/app/pages/dragonball/dragonball.ts
@@ -15,6 +15,9 @@ interface Character {
 })
 export class Dragonball {
 
+  name = signal('Gohan');
+  power = signal(7000);
+
   characters = signal<Character[]>([
     { name: 'Goku', power: 9000, id: 1 },
     { name: 'Vegeta', power: 8500, id: 2 },
@@ -22,4 +25,25 @@ export class Dragonball {
     { name: 'Frieza', power: 10000, id: 4 },
     { name: 'Yamcha', power: 500, id: 5 }
   ]);
+
+  addCharacter() {
+    if (!this.name() || !this.power() || this.power() <= 0) {
+      console.error('Invalid character data');
+      return;
+    }
+    const newCharacter: Character = {
+      name: this.name(),
+      power: this.power(),
+      id: this.characters().length + 1
+    };
+    console.log('Adding character:', newCharacter);
+    this.characters.update(chars => [...chars, newCharacter]);
+    this.resetForm();
+  }
+
+  resetForm() {
+    this.name.set('');
+    this.power.set(0);
+  }
+
 }
